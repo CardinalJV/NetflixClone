282b1b3c951ae1d865702bc6983cc3eaf1d70231
CardinalJV <jessy.viranaiken@icloud.com>
2024-10-16 16:21:26 +0200

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Jessy Viranaiken on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
