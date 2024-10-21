6c4eaf8e8658415fa363e1cc2e51501c6a1a5e2a
CardinalJV <jessy.viranaiken@icloud.com>
2024-10-21 19:09:07 +0200

    Update: Prise en compte de la fonction de trie

--- NetflixClone/Views/MoviesView.swift
+++ NetflixClone/Views/MoviesView.swift
@@ -1,9 +1,9 @@
-//
-//  MoviesView.swift
-//  NetflixClone
-//
-//  Created by Jessy Viranaiken on 18/10/2024.
-//
+  //
+  //  MoviesView.swift
+  //  NetflixClone
+  //
+  //  Created by Jessy Viranaiken on 18/10/2024.
+  //
 
 import SwiftUI
 
@@ -12,26 +12,29 @@ struct MoviesView: View {
   let moviesController: MoviesController
   
   var body: some View {
-    VStack{
-      ScrollView{
-        if !self.moviesController.moviesListsItems.isEmpty {
-          LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10),GridItem(.flexible(), spacing: 10)], spacing: 12.5){
-            ForEach(self.moviesController.moviesListsItems) { movie in
-              VStack{
-                ImageLoader(imageUrl: movie.posterPath!)
-                  .scaledToFit()
-                  .aspectRatio(contentMode: .fit)
-                  .frame(width: 112.5)
+    NavigationStack{
+      VStack{
+        ScrollView{
+          if !self.moviesController.moviesListsItems.isEmpty {
+            LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10),GridItem(.flexible(), spacing: 10)], spacing: 12.5){
+              ForEach(self.moviesController.selectedGenre != nil ? moviesController.sortByGenre()! : moviesController.moviesListsItems) { movieListItem in
+                  NavigationLink {
+                    MovieDetailsView(movieController: self.moviesController, movieListItem: movieListItem)
+                  } label: {
+                    ImageLoader(imageUrl: movieListItem.posterPath!)
+                      .scaledToFit()
+                      .aspectRatio(contentMode: .fit)
+                      .frame(width: 112.5)
+                  }
+                }
               }
-              .clipShape(RoundedRectangle(cornerRadius: 5))
             }
           }
         }
       }
     }
   }
-}
-//
-//#Preview {
-//    MoviesView()
-//}
+  //
+  //#Preview {
+  //    MoviesView()
+  //}
