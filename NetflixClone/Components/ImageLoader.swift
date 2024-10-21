dbfaea9ae5e656893171347c07f763e0166f2680
CardinalJV <jessy.viranaiken@icloud.com>
2024-10-21 19:03:36 +0200

    Update: Modification de la logique d'optionel

--- NetflixClone/Components/ImageLoader.swift
+++ NetflixClone/Components/ImageLoader.swift
@@ -9,25 +9,29 @@ import SwiftUI
 
 struct ImageLoader: View {
   
-  let imageUrl: URL
+  let imageUrl: URL?
   let baseURL = "https://image.tmdb.org/t/p/w500/"
   
     var body: some View {
-      AsyncImage(url: URL(string: self.baseURL + self.imageUrl.relativeString)) { phase in
-        switch phase {
-          case .empty:
-            ProgressView()
-          case .success(let image):
-            image
-              .resizable()
-          case .failure:
-            VStack{
-              Text("Erreur lors du chargement de l'image")
-              Image(systemName: "xmark.circle")
-            }
-          @unknown default:
-            EmptyView()
+      if self.imageUrl != nil {
+        AsyncImage(url: URL(string: self.baseURL + self.imageUrl!.relativeString)) { phase in
+          switch phase {
+            case .empty:
+              ProgressView()
+            case .success(let image):
+              image
+                .resizable()
+            case .failure:
+              VStack{
+                Text("Erreur lors du chargement de l'image")
+                Image(systemName: "xmark.circle")
+              }
+            @unknown default:
+              EmptyView()
+          }
         }
+      } else {
+        Image(systemName: "xmark")
       }
     }
 }
